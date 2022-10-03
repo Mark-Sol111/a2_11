c55fb4872321a5b4e91585b7197634d4427df9a3
Mark Sol <114338588+Mark-Sol111@users.noreply.github.com>
2022-10-04 10:01:26 +1100

    ++

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+
 
 #sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/wsgi
-sudo ln -s /home/box/etc/nginx-proxy.conf /etc/nginx/sites-enabled/nginx-proxy.conf
+rm /etc/nginx/sites-enabled/nginx-proxy.conf
+sudo cp /home/box/web/etc/nginx-proxy.conf  /etc/nginx/sites-enabled/nginx-proxy.conf
 sudo rm /etc/nginx/sites-enabled/default
 
 # gunicorn -c /etc/gunicorn.d/wsgi hello:app -D
