c76ff3627c0aabea1a02e1c28da7d0d581c1aa99
Mark Sol <114338588+Mark-Sol111@users.noreply.github.com>
2022-10-15 18:23:53 +1100

    del dir if exist

--- openssl_install.sh
+++ openssl_install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+DIR="/home/box/openssl"
+if [ -d "$DIR" ]; then
+  sudo rm -Rf $DIR
+fi
+
 cur=`pwd`
-td=/home/box/openssl
 sudo apt-get -y install make libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev libtk8.6 libgdm-dev libdb4o-cil-dev libpcap-dev
-mkdir $td && cd $td
+mkdir $DIR && cd $DIR
 wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz --no-check-certificate
 wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz.sha256 --no-check-certificate
 
@@ -23,7 +27,7 @@ echo "export PATH=/home/box/openssl/bin:$PATH
 export LD_LIBRARY_PATH=/home/box/openssl/lib
 export LC_ALL='en_US.UTF-8'
 export LDFLAGS='-L /home/box/openssl/lib -Wl,-rpath,/home/box/openssl/lib'
-" >> ~/.bash_profile
-source ~/.bash_profile
+" >> /home/box/.bashrc
 which openssl
+source ~/.bashrc
 cd $cur
