761f4d4f0d5013459a8686ca09058384e9f7a6a8
Mark Sol <114338588+Mark-Sol111@users.noreply.github.com>
2022-10-15 17:59:22 +1100

    fix del dir

--- mysql_install.sh
+++ mysql_install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+DIR="/home/box/mysql"
+if [ -d "$DIR" ]; then
+  sudo rm -Rf $DIR
+fi
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 cur=`pwd`
-mkdir ~/mysql && cd ~/mysql
+mkdir $DIRl && cd $DIR
 wget https://downloads.mysql.com/archives/get/p/23/file/mysql-server_5.7.9-1ubuntu14.04_amd64.deb-bundle.tar
 tar -xvf mysql-server_5.7.9-1ubuntu14.04_amd64.deb-bundle.tar
 sudo apt-get -y remove mysql-server
